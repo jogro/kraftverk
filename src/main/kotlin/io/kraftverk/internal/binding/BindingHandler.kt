60d27918c8b38dd04779c8e75d7a5cb09ef46212
jogro <jonas.gronberg@gmail.com>
2020-03-12 14:52:00 +0100

    Consolidating BindingHandler

--- src/main/kotlin/io/kraftverk/internal/binding/BindingHandler.kt
+++ src/main/kotlin/io/kraftverk/internal/binding/BindingHandler.kt
@@ -5,9 +5,14 @@
 
 package io.kraftverk.internal.binding
 
+import io.kraftverk.internal.logging.createLogger
+import io.kraftverk.internal.misc.intercept
+import io.kraftverk.internal.provider.Singleton
+import io.kraftverk.provider.BeanProviderImpl
 import io.kraftverk.provider.Provider
+import io.kraftverk.provider.ValueProviderImpl
 
-internal abstract class BindingHandler<T : Any>(config: BindingConfig<T>) {
+internal sealed class BindingHandler<T : Any>(config: BindingConfig<T>) {
 
     @Volatile
     internal var state: State<T> = State.Defining(config.copy())
@@ -25,5 +30,58 @@ internal abstract class BindingHandler<T : Any>(config: BindingConfig<T>) {
         ) : State<T>()
 
         object Destroyed : State<Nothing>()
+    }
+}
+
+internal class BeanHandler<T : Any>(config: BindingConfig<T>) : BindingHandler<T>(config) {
+
+    private val logger = createLogger { }
+
+    override fun createProvider(config: BindingConfig<T>): BeanProviderImpl<T> {
+        val loggingConfig = createLoggingConfig(config)
+        return BeanProviderImpl(
+            loggingConfig.name,
+            loggingConfig.lazy,
+            Singleton.of(loggingConfig)
+        )
+    }
+
+    private fun createLoggingConfig(config: BindingConfig<T>) =
+        config.copy().apply {
+            instance = intercept(instance) { proceed ->
+                val startMs = System.currentTimeMillis()
+                val t = proceed()
+                val elapsed = System.currentTimeMillis() - startMs
+                logger.info {
+                    "Bean '$name' is bound to $$type (${elapsed}ms)"
+                }
+                t
+            }
+        }
+}
+
+internal class ValueHandler<T : Any>(config: BindingConfig<T>) : BindingHandler<T>(config) {
+
+    private val logger = createLogger { }
+
+    override fun createProvider(config: BindingConfig<T>): ValueProviderImpl<T> {
+        val loggingConfig = createLoggingConfig(config)
+        return ValueProviderImpl(
+            loggingConfig.name,
+            loggingConfig.lazy,
+            Singleton.of(loggingConfig)
+        )
+    }
+
+    private fun createLoggingConfig(config: BindingConfig<T>) = config.copy().apply {
+        instance = intercept(instance) { proceed ->
+            val t = proceed()
+            if (secret) {
+                logger.info { "Value '$name' is bound to '********'" }
+            } else {
+                logger.info { "Value '$name' is bound to '$t'" }
+            }
+            t
+        }
     }
 }
