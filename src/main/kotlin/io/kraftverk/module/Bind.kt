8ead39ff9cded450da9fe407f10bef5263fb74a6
jogro <jonas.gronberg@gmail.com>
2020-03-13 16:41:26 +0100

    Fix typo

--- src/main/kotlin/io/kraftverk/module/Bind.kt
+++ src/main/kotlin/io/kraftverk/module/Bind.kt
@@ -76,8 +76,8 @@ class ValueBinder<T : Any> internal constructor(
     private val value: Value<T>
 ) {
     infix fun to(block: ValueSupplierDefinition<T>.() -> T) {
-        value.handler.bind { nextBind ->
-            ValueSupplierDefinition(container, nextBind).block()
+        value.handler.bind { process ->
+            ValueSupplierDefinition(container, process).block()
         }
     }
 }
