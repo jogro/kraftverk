9631398127f19b6348a960c964f558d589f55c7d
jogro <jonas.gronberg@gmail.com>
2020-03-14 18:05:04 +0100

    Copyright

--- src/main/kotlin/io/kraftverk/module/SubModule.kt
+++ src/main/kotlin/io/kraftverk/module/SubModule.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2019 Jonas Grönberg
+ * Licensed under MIT: https://github.com/jogro/kraftverk/blob/master/LICENSE
+ */
+
 package io.kraftverk.module
 
 import kotlin.properties.ReadOnlyProperty
